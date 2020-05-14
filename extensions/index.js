98c87924c649dc5da3372ebc3bc86869d7ac7c46
Zachary_Wu <andywu03242000@gmail.com>
2020-05-14 12:25:35 +0800

    修改註解

--- extensions/index.js
+++ extensions/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-//載入./Extension路徑下所有模組
+// 載入./extensions路徑下所有模組
 fs.readdirSync('./extensions').forEach(fileName => {
   fileName = fileName.replace('.js','');
   exports[fileName] = require('./' + fileName + '.js');  
