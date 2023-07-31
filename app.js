ec1ea18b2777923d45d3d382af737edc4a4748a9
Zachary Wu <andywu03242000@gmail.com>
2023-07-31 11:13:29 +0800

    FIX: .gitignore

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ dotenv.config();
 app.use(cors({
   origin: [
     'http://localhost:8080',
+    'http://blog.zachary.tw',
   ],
   methods: ['GET', 'POST'],
   credentials: true // enable set cookie
